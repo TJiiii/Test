84309913fd819b36874e8997aa59725fc6e62ed5
Attic Hermit <nomail@localhost>
2017-04-20 22:56:34 +0900

    fix big screen problem.

--- pythonprg/exec_this.py
+++ pythonprg/exec_this.py
@@ -1,5 +1,4 @@
 import sys
-sys.path.append("./brickbreak")
 
 print("실행하고자 하는 게임을 고르세요:")
 print("1. brickbreak")
@@ -8,10 +7,12 @@ print("3. pingpong")
 print("[1/2/3]: ", end="")
 a = input()
 if a=="1":
+    sys.path.append("./brickbreak")
     import brickbreak
 elif a == "2":
     import turtle_game
 elif a == "3":
+    sys.path.append("./pingpong")    
     import pingpong
 else:
     print("모르는 옵션: " + a)
